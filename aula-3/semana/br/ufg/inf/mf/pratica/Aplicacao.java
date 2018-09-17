8928eb6dcc59b6fe4eb2a3074fc572bc033766d6
unknown <murilo001os@gmail.com>
2018-09-17 15:41:16 -0300

    Aplicacao Semana

---
+++ aula-3/semana/br/ufg/inf/mf/pratica/Aplicacao.java
@@ -0,0 +1,11 @@
+package semana.br.ufg.inf.mf.pratica;
+
+import java.time.LocalDate;
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		System.out.println(LocalDate.now().getDayOfWeek());
+	}
+
+}
